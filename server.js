68a6788ecc725ae880fc2140422e815e2d307461
fawadaslam3377 <fwadaslam77@gmail.com>
2020-09-16 17:26:13 +0500

    all changes crash

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 "use strict";
 const http = require("http");
 const app = require("./app");
-const port = process.env.port || 3000;
+const port = process.env.port || 8080;
 const server = http.createServer(app);
 
 server.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}/`);
+  console.log(
+    `Listening on http://localhost:${port}/`,
+    process.env.NODE_ENV,
+    app.get("host"),
+    app.get("port")
+  );
 });
